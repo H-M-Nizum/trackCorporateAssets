87621d335701de69f5f9a4ae30d2fb47cb06344c
H-M-Nizum <hmnizum1714032@gmail.com>
2024-03-14 03:40:24 +0600

    Emplement search option, search by name company, employee and device

--- app/views.py
+++ app/views.py
@@ -7,13 +7,34 @@ class CompanyViews(viewsets.ModelViewSet):
     queryset = CompanyModel.objects.all()
     serializer_class = CompanySerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.query_params.get('name')
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
 class EmployeeViews(viewsets.ModelViewSet):
     queryset = EmployeeModel.objects.all()
     serializer_class = EmployeeSerializer
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.query_params.get('name')
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
 class DeviceViews(viewsets.ModelViewSet):
     queryset = DevicesModel.objects.all()
     serializer_class = DeviceSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.query_params.get('name')
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
 
 class DeviceAssignViews(viewsets.ModelViewSet):
     queryset = DevicesAssignModel.objects.all()
